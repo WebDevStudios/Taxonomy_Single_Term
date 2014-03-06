688d0c6ab0ab3a13adf1e80dfc32f5096d191484
Justin Sternberg <justin@dsgnwrks.pro>
2014-03-06 10:47:32 -0500

    Use a custom walker clone of `Walker_Category_Checklist` to get indented inputs

--- WDS_Taxonomy_Radio.class.php
+++ WDS_Taxonomy_Radio.class.php
@@ -1,176 +1,159 @@
 <?php
 
-if ( !class_exists( 'WDS_Taxonomy_Radio' ) ) {
+if ( ! class_exists( 'WDS_Taxonomy_Radio' ) ) :
+/**
+ * Removes and replaces the built-in taxonomy metabox with our radio-select metabox.
+ *
+ * Usage:
+ *
+ * $custom_tax_mb = new WDS_Taxonomy_Radio( 'custom-tax-slug' );
+ *
+ * Update optional properties:
+ *
+ * $custom_tax_mb->priority = 'low';
+ * $custom_tax_mb->context = 'normal';
+ * $custom_tax_mb->metabox_title = __( 'Custom Metabox Title', 'yourtheme' );
+ * $custom_tax_mb->force_selection = true;
+ * $custom_tax_mb->indented = false;
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
+ */
+class WDS_Taxonomy_Radio {
+
 	/**
-	 * Removes and replaces the built-in taxonomy metabox with our radio-select metabox.
-	 * @link  http://codex.wordpress.org/Function_Reference/add_meta_box#Parameters
+	 * Post types where metabox should be replaced (defaults to all post_types associated with taxonomy)
+	 * @var array
 	 */
-	class WDS_Taxonomy_Radio {
+	public $post_types = array();
 
-		/**
-		 * Post types where metabox should be replaced (defaults to all post_types associated with taxonomy)
-		 * @var array
-		 */
-		public $post_types = array();
-		
-		/**
-		 * Taxonomy slug
-		 * @var string
-		 */
-		public $slug = '';
-		
-		/**
-		 * Taxonomy object
-		 * @var object
-		 */
-		public $taxonomy = false;
-		
-		/**
-		 * New metabox title. Defaults to Taxonomy name
-		 * @var string
-		 */
-		public $metabox_title = '';
+	/**
+	 * Taxonomy slug
+	 * @var string
+	 */
+	public $slug = '';
 
-		/**
-		 * Metabox priority. (vertical placement)
-		 * 'high', 'core', 'default' or 'low'
-		 * @var string
-		 */
-		public $priority = 'high';
+	/**
+	 * Taxonomy object
+	 * @var object
+	 */
+	public $taxonomy = false;
 
-		/**
-		 * Metabox position. (column placement)
-		 * 'normal', 'advanced', or 'side'
-		 * @var string
-		 */
-		public $context = 'side';
-		
-		/**
-		 * Set to true to hide "None" option & force a term selection
-		 * @var boolean
-		 */
-		public $force_selection = false;
+	/**
+	 * New metabox title. Defaults to Taxonomy name
+	 * @var string
+	 */
+	public $metabox_title = '';
 
-		/**
-		 * Initiates our metabox action
-		 * @param string $tax_slug      Taxonomy slug
-		 * @param array  $post_types    post-types to display custom metabox
-		 */
-		public function __construct( $tax_slug, $post_types = array() ) {
+	/**
+	 * Metabox priority. (vertical placement)
+	 * 'high', 'core', 'default' or 'low'
+	 * @var string
+	 */
+	public $priority = 'high';
 
-			$this->slug = $tax_slug;
-			$this->post_types = is_array( $post_types ) ? $post_types : array( $post_types );
+	/**
+	 * Metabox position. (column placement)
+	 * 'normal', 'advanced', or 'side'
+	 * @var string
+	 */
+	public $context = 'side';
 
-			add_action( 'add_meta_boxes', array( $this, 'add_radio_box' ) );
+	/**
+	 * Set to true to hide "None" option & force a term selection
+	 * @var boolean
+	 */
+	public $force_selection = false;
+
+	/**
+	 * Whether hierarchical taxonomy inputs should be indented to represent hierarchy
+	 * @var boolean
+	 */
+	public $indented = true;
+
+	/**
+	 * Initiates our metabox action
+	 * @param string $tax_slug      Taxonomy slug
+	 * @param array  $post_types    post-types to display custom metabox
+	 */
+	public function __construct( $tax_slug, $post_types = array() ) {
+
+		$this->slug = $tax_slug;
+		$this->post_types = is_array( $post_types ) ? $post_types : array( $post_types );
+
+		add_action( 'add_meta_boxes', array( $this, 'add_radio_box' ) );
+	}
+
+	/**
+	 * Removes and replaces the built-in taxonomy metabox with our own.
+	 */
+	public function add_radio_box() {
+		// test the taxonomy slug construtor is an actual taxonomy
+		if ( ! $this->taxonomy() )
+			return;
+
+		foreach ( $this->post_types() as $key => $cpt ) {
+			// remove default category type metabox
+			remove_meta_box( $this->slug .'div', $cpt, 'side' );
+			// remove default tag type metabox
+			remove_meta_box( 'tagsdiv-'.$this->slug, $cpt, 'side' );
+			// add our custom radio box
+			add_meta_box( $this->slug .'_radio', $this->metabox_title(), array( $this, 'radio_box' ), $cpt, $this->context, $this->priority );
 		}
+	}
 
-		/**
-		 * Removes and replaces the built-in taxonomy metabox with our own.
-		 */
-		public function add_radio_box() {
-			// test the taxonomy slug construtor is an actual taxonomy
-			if ( ! $this->taxonomy() )
-				return;
-		
-			foreach ( $this->post_types() as $key => $cpt ) {
-				// remove default category type metabox
-				remove_meta_box( $this->slug .'div', $cpt, 'side' );
-				// remove default tag type metabox
-				remove_meta_box( 'tagsdiv-'.$this->slug, $cpt, 'side' );
-				// add our custom radio box
-				add_meta_box( $this->slug .'_radio', $this->metabox_title(), array( $this, 'radio_box' ), $cpt, $this->context, $this->priority );
-			}
-		}
+	/**
+	 * Displays our taxonomy radio box metabox
+	 */
+	public function radio_box() {
 
-		/**
-		 * Displays our taxonomy radio box metabox
-		 */
-		public function radio_box() {
+		// uses same noncename as default box so no save_post hook needed
+		wp_nonce_field( 'taxonomy_'. $this->slug, 'taxonomy_noncename' );
 
-			// uses same noncename as default box so no save_post hook needed
-			wp_nonce_field( 'taxonomy_'. $this->slug, 'taxonomy_noncename' );
+		require_once( 'WDS_Taxonomy_Radio_Walker.php' );
 
-			// get terms associated with this post
-			$names = wp_get_object_terms( get_the_ID(), $this->slug );
-			// get all terms in this taxonomy
-			$terms = (array) get_terms( $this->slug, 'hide_empty=0' );
-			// filter the ids out of the terms
-			$existing = ( !is_wp_error( $names ) && !empty( $names ) )
-				? (array) wp_list_pluck( $names, 'term_id' )
-				: array();
-			// Check if taxonomy is hierarchical
-			// Terms are saved differently between types
-			$h = $this->taxonomy()->hierarchical;
-
-			// default value
-			$default_val = $h ? 0 : '';
-			// input name
-			$name = $h ? 'tax_input['. $this->slug .'][]' : 'tax_input['. $this->slug .']';
-
-			echo '<div style="margin-bottom: 5px;">
-			<ul id="'. $this->slug .'_taxradiolist" data-wp-lists="list:'. $this->slug .'_tax" class="categorychecklist form-no-clear">';
-
-			// If 'category,' force a selection, or force_selection is true
-			if ( $this->slug != 'category' && !$this->force_selection ) {
-				// our radio for selecting none
-				echo '<li id="'. $this->slug .'_tax-0"><label><input value="'. $default_val .'" type="radio" name="'. $name .'" id="in-'. $this->slug .'_tax-0" ';
-				checked( empty( $existing ) );
-				echo '> '. sprintf( __( 'No %s', 'wds' ), $this->taxonomy()->labels->singular_name ) .'</label></li>';
-			}
-
-			// loop our terms and check if they're associated with this post
-			foreach ( $terms as $term ) {
-
-				$val = $h ? $term->term_id : $term->slug;
-
-				echo '<li id="'. $this->slug .'_tax-'. $term->term_id .'"><label><input value="'. $val .'" type="radio" name="'. $name .'" id="in-'. $this->slug .'_tax-'. $term->term_id .'" ';
-				// if so, they get "checked"
-				checked( !empty( $existing ) && in_array( $term->term_id, $existing ) );
-				echo '> '. $term->name .'</label></li>';
-			}
-			echo '</ul></div>';
-
-		}
-
-		/**
-		 * Gets the taxonomy object from the slug
-		 * @return object Taxonomy object
-		 */
-		public function taxonomy() {
-			$this->taxonomy = $this->taxonomy ? $this->taxonomy : get_taxonomy( $this->slug );
-			return $this->taxonomy;
-		}
-
-		/**
-		 * Gets the taxonomy's associated post_types
-		 * @return array Taxonomy's associated post_types
-		 */
-		public function post_types() {
-			$this->post_types = !empty( $this->post_types ) ? $this->post_types : $this->taxonomy()->object_type;
-			return $this->post_types;
-		}
-
-		/**
-		 * Gets the metabox title from the taxonomy object's labels (or uses the passed in title)
-		 * @return string Metabox title
-		 */
-		public function metabox_title() {
-			$this->metabox_title = !empty( $this->metabox_title ) ? $this->metabox_title : $this->taxonomy()->labels->name;
-			return $this->metabox_title;
-		}
-
+		$class = $this->indented ? $this->slug .'div' : $this->slug .'div-not-indented';
+		?>
+		<div class="<?php echo $class; ?> tabs-panel" style="margin-bottom: 5px;">
+			<ul id="<?php echo $this->slug; ?>checklist" data-wp-lists="list:<?php echo $this->slug?>" class="categorychecklist form-no-clear">
+				<?php wp_terms_checklist( get_the_ID(), array(
+					'taxonomy'      => $this->slug,
+					'checked_ontop' => false,
+					'walker'        => new WDS_Taxonomy_Radio_Walker( $this->taxonomy()->hierarchical ),
+				) ) ?>
+			</ul>
+		</div>
+		<?php
 
 	}
 
-	// Usage:
-	
-	// $custom_tax_mb = new WDS_Taxonomy_Radio( 'custom-tax-slug' );
-	
-	// Update optional properties
-	
-	// $custom_tax_mb->priority = 'low';
-	// $custom_tax_mb->context = 'normal';
-	// $custom_tax_mb->metabox_title = __( 'Custom Metabox Title', 'yourtheme' );
-	// $custom_tax_mb->force_selection = true;
+	/**
+	 * Gets the taxonomy object from the slug
+	 * @return object Taxonomy object
+	 */
+	public function taxonomy() {
+		$this->taxonomy = $this->taxonomy ? $this->taxonomy : get_taxonomy( $this->slug );
+		return $this->taxonomy;
+	}
+
+	/**
+	 * Gets the taxonomy's associated post_types
+	 * @return array Taxonomy's associated post_types
+	 */
+	public function post_types() {
+		$this->post_types = !empty( $this->post_types ) ? $this->post_types : $this->taxonomy()->object_type;
+		return $this->post_types;
+	}
+
+	/**
+	 * Gets the metabox title from the taxonomy object's labels (or uses the passed in title)
+	 * @return string Metabox title
+	 */
+	public function metabox_title() {
+		$this->metabox_title = !empty( $this->metabox_title ) ? $this->metabox_title : $this->taxonomy()->labels->name;
+		return $this->metabox_title;
+	}
+
 
 }
+
+endif; // class_exists check
---
+++ WDS_Taxonomy_Radio_Walker.php
@@ -0,0 +1,104 @@
+<?php
+
+if ( ! class_exists( 'WDS_Taxonomy_Radio_Walker' ) && class_exists( 'Walker' ) ) :
+
+/**
+ * Walker to output an unordered list of category checkbox <input> elements.
+ *
+ * @see Walker
+ * @see wp_category_checklist()
+ * @see wp_terms_checklist()
+ * @since 2.5.1
+ */
+class WDS_Taxonomy_Radio_Walker extends Walker {
+	var $tree_type = 'category';
+	var $db_fields = array ('parent' => 'parent', 'id' => 'term_id'); //TODO: decouple this
+
+	function __construct( $hierarchical ) {
+		$this->hierarchical = $hierarchical;
+	}
+
+	/**
+	 * Starts the list before the elements are added.
+	 *
+	 * @see Walker:start_lvl()
+	 *
+	 * @since 2.5.1
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth  Depth of category. Used for tab indentation.
+	 * @param array  $args   An array of arguments. @see wp_terms_checklist()
+	 */
+	function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent<ul class='children'>\n";
+	}
+
+	/**
+	 * Ends the list of after the elements are added.
+	 *
+	 * @see Walker::end_lvl()
+	 *
+	 * @since 2.5.1
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param int    $depth  Depth of category. Used for tab indentation.
+	 * @param array  $args   An array of arguments. @see wp_terms_checklist()
+	 */
+	function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent</ul>\n";
+	}
+
+	/**
+	 * Start the element output.
+	 *
+	 * @see Walker::start_el()
+	 *
+	 * @since 2.5.1
+	 *
+	 * @param string $output   Passed by reference. Used to append additional content.
+	 * @param object $category The current term object.
+	 * @param int    $depth    Depth of the term in reference to parents. Default 0.
+	 * @param array  $args     An array of arguments. @see wp_terms_checklist()
+	 * @param int    $id       ID of the current term.
+	 */
+	function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+		extract($args);
+
+		if ( empty( $taxonomy ) )
+			$taxonomy = 'category';
+
+		if ( $taxonomy == 'category' )
+			$name = 'post_category';
+		else
+			$name = 'tax_input['.$taxonomy.']';
+
+		// input name
+		$name = $this->hierarchical ? $name .'[]' : $name;
+		// input value
+		$val  = $this->hierarchical ? $category->term_id : $category->slug;
+
+		$class = in_array( $category->term_id, $popular_cats ) ? ' class="popular-category"' : '';
+		$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" . '<label class="selectit"><input value="' . $val . '" type="radio" name="'.$name.'" id="in-'.$taxonomy.'-' . $category->term_id . '"' . checked( in_array( $category->term_id, $selected_cats ), true, false ) . disabled( empty( $args['disabled'] ), false, false ) . ' /> ' . esc_html( apply_filters('the_category', $category->name )) . '</label>';
+
+	}
+
+	/**
+	 * Ends the element output, if needed.
+	 *
+	 * @see Walker::end_el()
+	 *
+	 * @since 2.5.1
+	 *
+	 * @param string $output   Passed by reference. Used to append additional content.
+	 * @param object $category The current term object.
+	 * @param int    $depth    Depth of the term in reference to parents. Default 0.
+	 * @param array  $args     An array of arguments. @see wp_terms_checklist()
+	 */
+	function end_el( &$output, $category, $depth = 0, $args = array() ) {
+		$output .= "</li>\n";
+	}
+}
+
+endif; // class_exists check
